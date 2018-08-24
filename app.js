248c82693c9871a7c2cd9e1b4b003cafbf3fcd75
Patrick Lafferty <patrick.lafferty@slalom.com>
2018-08-24 12:00:06 -0400

    adjusted app.js

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-/* catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-*/
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
