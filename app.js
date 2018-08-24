de987c3adbf908c39d073587fd18d23fd901fbc1
patlaff <patlaff728@gmail.com>
2018-08-24 01:03:36 -0400

    Serve stylesheets statically with nginx.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
