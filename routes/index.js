8d255f3f16e0f1326c8a5e43ec9b2612f1756b0b
Patrick Lafferty <patrick.lafferty@slalom.com>
2018-08-24 11:54:06 -0400

    removed error handling from index.js

--- routes/index.js
+++ routes/index.js
@@ -97,20 +97,4 @@ app.listen(port, function () {
   console.log('App listening on port ' + port +'!');
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = router;
